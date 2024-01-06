70959378e3ec0f78410af793e47fb526e981c7c5
bonuwa <b.usowicz@gmail.com>
2024-01-06 16:17:00 +0100

    First pandas code

--- thermal.py
+++ thermal.py
@@ -1,5 +1,17 @@
-a = 2
-b = 4
-c = a + b
+import pandas as pd
 
-print(f"Wynik to: {c}")
+df = pd.read_excel(".\input.xlsx")
+
+print(df)
+
+df2 = df.map(lambda e: e + 10)
+
+mw = df.mean(axis='index')
+print(mw)
+
+df2.to_csv('.\output.csv')
+
+df3 = pd.read_csv(".\output.csv")
+
+pd.read
+print(df3)
